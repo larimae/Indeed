7d8f121a65789b0df2f96fd56c935df15b86dc01
larimae <larissamae60@gmail.com>
2024-11-13 19:26:36 -0800

    added candidatesearch code

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+console.log('Vite is building with this config...');
+
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: './index.html', // Path to the entry point
+    },
+    outDir: 'dist', // Output directory
+  },
 });
